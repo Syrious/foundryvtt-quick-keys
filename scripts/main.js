77d02988e376d9b7009e2e96f852a841d22f614d
dbrinkmann <dbrinkmann@neusta.de>
2024-04-12 09:05:22 +0200

    Adds three-tap-pinning

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,43 @@
+import {maxDelay} from "./settings.js";
+
 CONFIG.debug.hooks = false;
 
-let lastActive
+let tabToSwitchBackTo
+let lastManuallyActivatedTab
 let bodyElement;
 
+let keyPressTimestamps = [];
 
 Hooks.once('ready', async () => {
     bodyElement = document.querySelector('.vtt.game');
 });
 
-export function change(param) {
+export function change(viewToChangeTo) {
     if (document.activeElement === bodyElement) {
-        //  lastActive = document.querySelector('.scene-control.active').dataset.control;
-        lastActive = ui.controls.activeControl;
-        activateElement(param);
+        //  tabToSwitchBackTo = document.querySelector('.scene-control.active').dataset.control;
+        tabToSwitchBackTo = ui.controls.activeControl;
+
+        accessibility();
+        activateElement(viewToChangeTo);
+        countTaps();
     }
 }
 
 export function restore() {
-    activateElement(lastActive);
-    lastActive = null;
+    if (tabToSwitchBackTo) {
+        activateElement(tabToSwitchBackTo);
+        tabToSwitchBackTo = null;
+    }
+}
+
+function accessibility() {
+    // This checks if the user manually switched to another tab in the meantime.
+    // This consideration is especially essential for users who may need additional time to navigate due to accessibility needs ♥
+    // If the user has switched tabs, we reset the taps counter and update the last manually activated tab.
+    if (tabToSwitchBackTo !== lastManuallyActivatedTab) {
+        lastManuallyActivatedTab = tabToSwitchBackTo;
+        resetTapsCounter();
+    }
 }
 
 function activateElement(param) {
@@ -28,4 +47,23 @@ function activateElement(param) {
     } else {
         console.log(`element ${param} not found`)
     }
+}
+
+function countTaps() {
+    keyPressTimestamps.push(Date.now());
+
+    if (keyPressTimestamps.length > 3) {
+        keyPressTimestamps.shift();  // Keep only last 3 timestamps
+    }
+
+    const delay = keyPressTimestamps[2] - keyPressTimestamps[0];
+    if (keyPressTimestamps.length === 3 && delay <= maxDelay) {
+        // Key has been quickly tapped 3 times in a row.
+        tabToSwitchBackTo = null;
+        resetTapsCounter()
+    }
+}
+
+function resetTapsCounter() {
+    keyPressTimestamps = [];
 }
---
+++ scripts/settings.js
@@ -0,0 +1,25 @@
+Hooks.on('init', () => {
+    console.warn("Registering Keys for quick keys")
+    let moduleId = "quick-keys";
+
+    game.settings.register(moduleId, "delay-settings", {
+        name: "Pin delay",
+        label: "Pin delay",
+        hint: "The time between three taps to pin the view",
+        scope: "client",
+        config: true,
+        default: 500,
+        type: Number,
+        requiresReload: false,
+        onChange: value => {
+            maxDelay = value;
+        }
+    });
+
+});
+
+Hooks.once('ready', async () => {
+  maxDelay = game.settings.get("quick-keys", "delay-settings");
+});
+
+export let maxDelay;
